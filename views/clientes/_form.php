1dda7faea9197873769a380ddf301c9970f77f9d
Juan Antonio Jerez <juanantonio.jerez@iesdonana.org>
2020-01-19 10:56:34 +0100

    Generador de crud Clientes

---
+++ views/clientes/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\bootstrap4\Html;
+use yii\bootstrap4\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Clientes */
+/* @var $form yii\bootstrap4\ActiveForm */
+?>
+
+<div class="clientes-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'telefono')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'direccion')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'nota')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ views/clientes/index.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\bootstrap4\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\ClientesSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Clientes';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="clientes-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Clientes', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'nombre',
+            'telefono',
+            'direccion',
+            'nota',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
